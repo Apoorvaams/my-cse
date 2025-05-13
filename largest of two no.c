056e0858f0b1ae64c8c2e000bb89e29ca82080ff
Apoorvaams <apoorvams624@gmail.com>
2025-05-13 16:11:39 +0530

    Create largest of two no.c

---
+++ largest of two no.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int num1,num2;
+    printf("enter two numbers:");
+    scanf("%d,%d",&num1,&num2);
+    if (num1>num2){
+        printf("the largest number is:%d\n",num1);
+    }
+    else{
+        printf("the largest number is:%d\n",num2);
+    }
+    return 0;
+}
+    
