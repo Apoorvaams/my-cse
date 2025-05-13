2445be32e299f11de33b8ba9b6b48be076426ab3
Apoorvaams <apoorvams624@gmail.com>
2025-05-13 15:40:11 +0530

    Create function bio.c

---
+++ function bio.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int siet()
+{
+   printf("apoorva\n");
+   printf("12/4/2006\n");
+   printf("19\n");
+   printf("8088608716\n");
+}
+int main()
+{
+    siet();
+    return 0;
+}
