792d4c43d2807e53dc0a35503cb2f37d14e583cb
Apoorvaams <apoorvams624@gmail.com>
2025-05-13 15:25:36 +0530

    Create function.c

---
+++ function.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int add(int a, int b){
+    int c=a+b;
+    return c;}
+    int main(){
+        int result=add(2,5);
+        printf("the result is %d",result);
+        return 0;
+    }
