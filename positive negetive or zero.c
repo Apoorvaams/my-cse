c4fe532df4120463f0912de3ad6944d0ab6f9e32
Apoorvaams <apoorvams624@gmail.com>
2025-05-13 16:19:25 +0530

    Create positive negetive or zero.c

---
+++ positive negetive or zero.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("enter the number");
+    scanf("%d",&num);
+    if(num>0){
+        printf("the number is positive");
+    }
+    else if(num<0){
+        printf("the number is negative");
+    }
+    else if(num=0){
+        printf("the number is 0");
+        }
+    }
