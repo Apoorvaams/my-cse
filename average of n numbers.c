c2906d35a154413419774d2598622273c03156be
Apoorvaams <apoorvams624@gmail.com>
2025-05-14 21:10:41 +0530

    Create average of n numbers.c

---
+++ average of n numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int n=5,i,sum=0;
+int average()
+{
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        sum=sum+arr[i];
+    }
+    int avg=sum/n;
+    return avg;
+}
+int main()
+{
+    printf("enter 5 elements to array:");
+    int res=average();
+    printf("average of all array elements is:%d",res);
+}
