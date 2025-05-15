a1ff2d736129333bf25703d0f73a710f2c5e9e76
Apoorvaams <apoorvams624@gmail.com>
2025-05-15 19:30:19 +0530

    Create pass string to function .c

---
+++ pass string to function .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int i,n;
+int each(char arr[n])
+{
+    for(i=0;i<n;i++)
+    {
+        printf("%c",arr[i]);
+    }
+}
+int main()
+{
+    char arr[]={"apoorva"};
+    n=strlen(arr);
+    int res=each(arr);
+}
